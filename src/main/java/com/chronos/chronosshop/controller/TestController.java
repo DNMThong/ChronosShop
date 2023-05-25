4ed1573cac13783c22e0f29dba658b14641a550c
NguyenHoangQuanke <100291264+NguyenHoangQuanke@users.noreply.github.com>
2023-05-25 20:25:16 +0700

    add admin page, admin component.v.v

--- src/main/java/com/chronos/chronosshop/controller/TestController.java
+++ src/main/java/com/chronos/chronosshop/controller/TestController.java
@@ -14,5 +14,47 @@ public class TestController {
     @RequestMapping("/login")
     public String login() {return "page/login-page";};
 
+    @RequestMapping("/addcategory")
+    public String addcategory() {
+        return "admin/addcategory";
+    }
+    @RequestMapping("/addbrand")
+    public String addbrand() {
+        return "admin/addbrand";
+    }
+    @RequestMapping("/add-sales")
+    public String addsales() {
+        return "admin/add-sales";
+    }
+    @RequestMapping("/addproduct")
+    public String addproduct() {
+        return "admin/addproduct";
+    }
+    @RequestMapping("/addcustomer")
+    public String addcustomer() {
+        return "admin/addcustomer";
+    }
+    @RequestMapping("/adduser")
+    public String adduser() {
+        return "admin/adduser";
+    }
+    @RequestMapping("/brandlist")
+    public String brandlist() {
+        return "admin/brandlist";
+    }
+
+    //    t----t
+    @RequestMapping("/activities")
+    public String activities() {
+        return "admin/activities";
+    }
+    @RequestMapping("/productlist")
+    public String productlist() {
+        return "admin/productlist";
+    }
+    @RequestMapping("/categorylist")
+    public String categorylist() {
+        return "admin/categorylist";
+    }
 
 }
