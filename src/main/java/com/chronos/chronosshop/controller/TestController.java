91accb02a85cd4939d0cd0417cc042b4b04725e2
Whisper <nghia14802@gmail.com>
2023-05-26 21:02:36 +0700

    complete fill category, change syntax fragment

--- src/main/java/com/chronos/chronosshop/controller/TestController.java
+++ src/main/java/com/chronos/chronosshop/controller/TestController.java
@@ -1,61 +1,106 @@
 package com.chronos.chronosshop.controller;
 
+import com.chronos.chronosshop.entity.Category;
+import com.chronos.chronosshop.repository.CategoryRepository;
+import com.chronos.chronosshop.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("test")
 public class TestController {
-    @RequestMapping("/")
+
+//    CategoryService categoryService;
+
+//    List<Category> categoryList = null;
+
+//    @Autowired
+//    public TestController(CategoryService categoryService) {
+//        this.categoryService = categoryService;
+//    }
+
+
+    @RequestMapping("")
     public String index() {
         return "page/home-page";
     }
 
-    @RequestMapping("/productItem")
+    @RequestMapping("productItem")
     public String productItem() {
         return "page/productItem";
     }
 
-    @RequestMapping("/login")
-    public String login() {return "page/login-page";};
+    @RequestMapping("login")
+    public String login(Model model) {
+        return "page/login-page";};
 
-    @RequestMapping("/addcategory")
+    @RequestMapping("sign-up")
+    public String signUp(Model model) {
+
+        return "page/sign-up-page";}
+
+//    @ModelAttribute("femaleCategories")
+//    public List<Category> getFemaleCategory() {
+//        categoryList = categoryService.findCategoriesByName("Nữ");
+//        return categoryList;
+//    }
+
+//    @ModelAttribute("maleCategories")
+//    public List<Category> getMaleCategory() {
+//        categoryList = categoryService.findCategoriesByName("Nam");
+//        return categoryList;
+//    }
+
+//    @ModelAttribute("kidCategories")
+//    public List<Category> getKidCategory() {
+//        categoryList = categoryService.findCategoriesByName("Trẻ em");
+//        return categoryList;
+//    }
+
+    @RequestMapping("addcategory")
     public String addcategory() {
         return "admin/addcategory";
     }
-    @RequestMapping("/addbrand")
+    @RequestMapping("addbrand")
     public String addbrand() {
         return "admin/addbrand";
     }
-    @RequestMapping("/add-sales")
+    @RequestMapping("add-sales")
     public String addsales() {
         return "admin/add-sales";
     }
-    @RequestMapping("/addproduct")
+    @RequestMapping("addproduct")
     public String addproduct() {
         return "admin/addproduct";
     }
-    @RequestMapping("/addcustomer")
+    @RequestMapping("addcustomer")
     public String addcustomer() {
         return "admin/addcustomer";
     }
-    @RequestMapping("/adduser")
+    @RequestMapping("adduser")
     public String adduser() {
         return "admin/adduser";
     }
-    @RequestMapping("/brandlist")
+    @RequestMapping("brandlist")
     public String brandlist() {
         return "admin/brandlist";
     }
 
-    @RequestMapping("/activities")
+    @RequestMapping("activities")
     public String activities() {
         return "admin/activities";
     }
-    @RequestMapping("/productlist")
+    @RequestMapping("productlist")
     public String productlist() {
         return "admin/productlist";
     }
-    @RequestMapping("/categorylist")
+    @RequestMapping("categorylist")
     public String categorylist() {
         return "admin/categorylist";
     }
