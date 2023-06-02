0fad49db7cd978cf7e2450dafbed5d30ba012ea7
QUOC AN <nguyenvoquocan1112@gmail.com>
2023-06-02 15:43:22 +0700

    fragment_admin(link, header, script)

--- src/main/java/com/chronos/chronosshop/controller/admin/UserAdminController.java
+++ src/main/java/com/chronos/chronosshop/controller/admin/UserAdminController.java
@@ -1,18 +1,24 @@
 package com.chronos.chronosshop.controller.admin;
 
+import com.chronos.chronosshop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin/user")
 public class UserAdminController {
+    @Autowired
+    UserService userService;
     @GetMapping("/add")
     public String addUser(){
         return"admin/adduser";
     }
     @GetMapping("/list")
-    public String userList(){
+    public String getAllUser(Model model){
+        model.addAttribute("listUser", userService.listAll());
         return"admin/userlists";
     }
 }
