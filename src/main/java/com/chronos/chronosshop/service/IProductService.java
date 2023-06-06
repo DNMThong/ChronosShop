27bcf95145718cbd6cdc2ecd45c6e27313854e5e
dnmthong <duongthong07@gmail.com>
2023-06-06 21:16:30 +0700

    refactor service

--- src/main/java/com/chronos/chronosshop/service/IProductService.java
+++ src/main/java/com/chronos/chronosshop/service/IProductService.java
@@ -2,5 +2,8 @@ package com.chronos.chronosshop.service;
 
 import com.chronos.chronosshop.entity.Product;
 
+import java.util.List;
+
 public interface IProductService extends IService<Product,Integer>{
+
 }
